0db33e90472c53a84867a9fdf3e45844dc032ce4
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-06-22 18:04:03 -0300

    feat: bottom app bar

---
+++ fitnesspro-compose-components/src/main/java/br/com/fitnesspro/compose/components/bottombar/BottomAppBar.kt
@@ -0,0 +1,31 @@
+package br.com.fitnesspro.compose.components.bottombar
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import br.com.fitnesspro.core.theme.GREY_400
+
+/**
+ * Componente bottom app bar padrão.
+ *
+ * @param modifier
+ * @param actions
+ * @param floatingActionButton
+ *
+ * @author Nikolas Luiz Schmitt
+ */
+@Composable
+fun FitnessProBottomAppBar(
+    modifier: Modifier = Modifier,
+    actions: @Composable RowScope.() -> Unit = { },
+    floatingActionButton: @Composable () -> Unit = { }
+) {
+    BottomAppBar(
+        modifier = modifier,
+        actions = actions,
+        floatingActionButton = floatingActionButton,
+        containerColor = GREY_400
+    )
+}
