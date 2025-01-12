a8e997a7e2c617e96226881385ddf678affd0ce3
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-01-12 13:35:13 -0300

    fix: ajuste na quantidade de caracteres do e-mail

--- fitnesspro-common-module/src/main/java/br/com/fitnesspro/common/usecase/person/EnumValidatedPersonFields.kt
+++ fitnesspro-common-module/src/main/java/br/com/fitnesspro/common/usecase/person/EnumValidatedPersonFields.kt
@@ -5,7 +5,7 @@ import br.com.fitnesspro.common.R
 
 enum class EnumValidatedPersonFields(val labelResId: Int, val maxLength: Int = 0) {
     NAME(R.string.label_person_name, 512),
-    EMAIL(R.string.label_person_email, 512),
+    EMAIL(R.string.label_person_email, 64),
     PASSWORD(R.string.label_person_password, 1024),
     BIRTH_DATE(R.string.label_person_birth_date),
     PHONE(R.string.label_person_phone, 11)
