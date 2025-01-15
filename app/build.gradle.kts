f64913bc0813000ecab1d1ace1be23eb34ad631d
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-01-15 17:56:06 -0300

    adicionando key store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("KEYSTORE_FILE"))
+            storeFile = file(System.getenv("KEYSTORE_FILE") ?: throw GradleException("Keystore file is missing"))
             storePassword = System.getenv("KEYSTORE_PASSWORD")
             keyAlias = System.getenv("KEY_ALIAS")
             keyPassword = System.getenv("KEY_PASSWORD")
