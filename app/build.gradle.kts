e1b00c6282b0064cdf5902f1165403d316847c24
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-01-15 17:49:11 -0300

    adicionando key store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("KEYSTORE_FILE"))
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
