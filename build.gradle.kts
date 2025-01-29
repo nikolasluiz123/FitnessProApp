eb02fc04af58bc5f4550304d1d0cc94dcf320f79
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-01-29 17:33:30 -0300

    fix: ajustes para envio de performance para o firebase
    Issue #7

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
+    alias(libs.plugins.firebase.performance) apply false
 }
