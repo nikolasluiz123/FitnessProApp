a8a67067dc486b6b4bed7850f33cabf925da95e9
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-02-20 23:13:27 -0300

    fix: correção na consulta que verifica se já existe um usuário com o e-mail informado

--- fitnesspro-local-data-access/src/main/java/br/com/fitnesspro/local/data/access/dao/UserDAO.kt
+++ fitnesspro-local-data-access/src/main/java/br/com/fitnesspro/local/data/access/dao/UserDAO.kt
@@ -2,15 +2,52 @@ package br.com.fitnesspro.local.data.access.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.RawQuery
 import androidx.room.Transaction
+import androidx.sqlite.db.SimpleSQLiteQuery
+import androidx.sqlite.db.SupportSQLiteQuery
 import br.com.fitnesspro.local.data.access.dao.common.MaintenanceDAO
 import br.com.fitnesspro.model.general.User
+import java.util.StringJoiner
 
 @Dao
 abstract class UserDAO: MaintenanceDAO<User>() {
 
-    @Query("select exists(select 1 from user where email = :email and id != :userId)")
-    abstract suspend fun hasUserWithEmail(email: String, userId: String?): Boolean
+    suspend fun hasUserWithEmail(email: String, userId: String?): Boolean {
+        val queryParams = mutableListOf<Any>()
+
+        val select = StringJoiner(QR_NL).apply {
+            add(" select 1 ")
+        }
+
+        val from = StringJoiner(QR_NL).apply {
+            add(" from user ")
+        }
+
+        val where = StringJoiner(QR_NL).apply {
+            add(" where user.active = 1 ")
+            add(" and user.email = ?  ")
+            queryParams.add(email)
+
+            userId?.let {
+                add(" and user.id != ? ")
+                queryParams.add(it)
+            }
+        }
+
+        val sql = StringJoiner(QR_NL).apply {
+            add(" select exists ( ")
+            add(select.toString())
+            add(from.toString())
+            add(where.toString())
+            add(" ) ")
+        }
+
+        return executeQueryHasUserWithEmail(SimpleSQLiteQuery(sql.toString(), queryParams.toTypedArray()))
+    }
+
+    @RawQuery
+    abstract suspend fun executeQueryHasUserWithEmail(query: SupportSQLiteQuery): Boolean
 
     @Query("select exists (select 1 from user where email = :email and password = :hashedPassword)")
     abstract suspend fun hasUserWithCredentials(email: String, hashedPassword: String): Boolean
