ce59677a0c543916dfa9103f7a1b65c8223d748a
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-05-11 14:37:16 -0300

    fix: correções no manifest para o app aparecer novamente nos aplicativos do android

--- app/src/main/java/br/com/fitnesspro/AndroidTestsActivity.kt
+++
@@ -1,7 +0,0 @@
-package br.com.fitnesspro
-
-import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class AndroidTestsActivity: ComponentActivity()
