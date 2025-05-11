3827effc26fa5ee3cbfcd30dee67f5eb18b890dc
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-05-11 20:47:48 -0300

    build dos apks de debug e release além da criação de tag e release no github

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,6 +18,18 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         mavenLocal()
+
+        maven {
+            name = "GitHubPackages" // Um nome descritivo
+            url = uri("https://maven.pkg.github.com/nikolasluiz123/FitnessProService") // URL do seu repositório onde o pacote foi publicado
+            credentials {
+                // Use providers.gradleProperty para acessar propriedades do Gradle
+                // OrNull para obter o valor String ou null se a propriedade não existir
+                // O fallback para System.getenv é bom para CI, onde variáveis podem ser setadas diretamente
+                username = providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")
+                password = providers.gradleProperty("gpr.key").orNull ?: System.getenv("PACKAGES_TOKEN")
+            }
+        }
     }
 }
 
