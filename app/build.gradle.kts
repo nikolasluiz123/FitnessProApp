3a40a61a76f2ab6860491f922aacb76d72d4276c
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-11-04 20:52:04 -0300

    [versão] ajuste de versão
    Issue #50

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.3.3"
+        versionName = "2.3.4"
 
         android.buildFeatures.buildConfig = true
         vectorDrawables {
