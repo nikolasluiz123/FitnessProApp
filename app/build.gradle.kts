241ef15ab6bce22e4e3e87858467e79a6413c2ec
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2025-11-07 19:33:22 -0300

    [versão] ajuste de versão do app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.3.5"
+        versionName = "2.3.6"
 
         android.buildFeatures.buildConfig = true
         vectorDrawables {
